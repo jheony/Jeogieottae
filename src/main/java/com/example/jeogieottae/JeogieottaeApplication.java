1d37766b0cd12740d3c01103be8160424bebf54c
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-31 21:56:25 +0900

    Initial commit

---
+++ src/main/java/com/example/jeogieottae/JeogieottaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeogieottae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeogieottaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeogieottaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jeogieottae/JeogieottaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeogieottae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeogieottaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
